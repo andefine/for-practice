1d8c659edde24668d99237f6838d8c976b0797cc
andefine <1939866551@qq.com>
2018-08-07 10:04:27 +0800

    通过Prop向子组件传递数据

--- vueInstance/vueInstance.js
+++ vueInstance/vueInstance.js
@@ -1,10 +1,6 @@
-Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }}</button>'
+Vue.component('blog-post', {
+  props: ['title'],
+  template: '<h3>{{ title }}</h3>'
 })
 
 new Vue({
