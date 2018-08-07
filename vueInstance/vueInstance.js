824eda0c93491c69b3de8714102bdc2099d4f78c
andefine <1939866551@qq.com>
2018-08-07 10:13:18 +0800

    v-bind传递prop

--- vueInstance/vueInstance.js
+++ vueInstance/vueInstance.js
@@ -4,5 +4,12 @@ Vue.component('blog-post', {
 })
 
 new Vue({
-  el: '#example'
+  el: '#example',
+  data: {
+    posts: [
+      { id: 1, title: 'my journey with vue' },
+      { id: 2, title: 'blogging with vue' },
+      { id: 3, title: 'why vue is so fun' }
+    ]
+  }
 })
