76aa0e34384b97385bdc15e104f137d852a7677a
andefine <1939866551@qq.com>
2018-08-07 11:02:01 +0800

    通过事件向父级组件发送消息

--- vueInstance/vueInstance.js
+++ vueInstance/vueInstance.js
@@ -3,6 +3,7 @@ Vue.component('blog-post', {
   template: `
     <div class="blog-post">
       <h3>{{ post.title }}</h3>
+      <button v-on:click="$emit('enlarge-text')">enlarge text</button>
       <div v-html="post.content"></div>
     </div>
   `
@@ -15,6 +16,7 @@ new Vue({
       { id: 1, title: 'my journey with vue' },
       { id: 2, title: 'blogging with vue' },
       { id: 3, title: 'why vue is so fun' }
-    ]
+    ],
+    postFontSize: 1
   }
 })
